a1af118d2c81a471778df84115437de40e23b039
jeff <jefferykibet@gmail.com>
2026-06-30 12:15:50 +0300

    Improve login tracker feedback messages and formatting

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-function createLoginTracker(userInfo){
-  let attemptCount = 0 
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
   return (passwordAttempt) => {
     attemptCount++;
-    if(attemptCount > 3){
-      return "Account locked due to too many attempts";
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
     }
     if (passwordAttempt === userInfo.password) {
       return "Login successful";
     } else {
-      return "Attempt ${attemptCount}:Login failed";
+      return `Attempt ${attemptCount}: Login failed`;
     }
   };
 }
 
-
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
