66d4dcbdc5bd5664b87d8907d3c26b9a3402fcf0
jeff <jefferykibet@gmail.com>
2026-06-30 12:02:58 +0300

    Implement login tracker function with attempt limit and feedback messages

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-
+function createLoginTracker(userInfo){
+  let attemptCount = 0 
+  return (passwordAttempt) => {
+    attemptCount++;
+    if(attemptCount > 3){
+      return "Account locked due to too many attempts";
+    }
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      return ' Login attempt ${attemptCount}: Login failed';
+    }
+  }
+}
 
 
 module.exports = {
